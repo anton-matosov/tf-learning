909d6868f964a5f36c6ea4496f3dec25d807d146
Anton Matosov <anton.matosov@gmail.com>
2019-06-21 20:52:21 -0700

    fix qnet to use proper activation and do max

--- agents/network_agent.py
+++ agents/network_agent.py
@@ -2,14 +2,19 @@ from . abstract_agent import AbstractAgent
 
 import tensorflow as tf
 
+# Experience = collections.namedtuple('Experience', field_names=['observation', 'action', 'reward', 'done', 'new_state'])
+
 class NetworkAgent(AbstractAgent):
   def __init__(self, env, network):
     super().__init__(env)
 
     self.network = network
     self.network.configure(self.observation_space, self.action_space)
-    
 
   def select_action(self, observation):
-    return self.network.forward_pass(observation)
+    q_values = self.network.forward_pass(observation)
+    return tf.math.argmax(q_values[0]).numpy()
+
+  def register_reward(self, observation, reward, done):
+    pass
 
--- networks/feed_fwd.py
+++ networks/feed_fwd.py
@@ -11,38 +11,34 @@ class Network():
   def learn(self):
     pass
 
-  def configure(self, observation_shape, action_shape, output_activation = tf.nn.softmax):
+  def configure(self, observation_shape, action_shape, output_activation = None):
     if isinstance(observation_shape, gym.spaces.Box):
       input_shape = observation_shape.shape
 
     if isinstance(action_shape, gym.spaces.Discrete):
       num_output_neurons = action_shape.n
       self._discrete = True
-    self._configure(input_shape, num_output_neurons)
+
+    self._configure(input_shape, num_output_neurons, output_activation)
 
   def forward_pass(self, inputs):
-    activations = self._forward_pass(np.array([inputs]))
-    if self._discrete:
-      return tf.random.categorical(activations, num_samples=1).numpy().flatten()[0]
-    else:
-      return activations
-    
+    self.activations = self._forward_pass(np.array([inputs]))
+    return self.activations
 
 class FeedForwardNetwork(Network):
   def __init__(self, hidden_layers):
     super(FeedForwardNetwork, self).__init__()
     self.hidden_layers = hidden_layers
     
-  def _configure(self, input_shape, num_output_neurons, output_activation = tf.nn.softmax):
-    self.input_layer = layers.Input(input_shape)
+  def _configure(self, input_shape, num_output_neurons, output_activation = None):
+    self.model = tf.keras.Sequential()
 
-    last_layer = tf.keras.layers.Flatten()(self.input_layer)
+    self.model.add(layers.Input(input_shape))
+    self.model.add(tf.keras.layers.Flatten())
     for num_neurons in self.hidden_layers:
-      last_layer = layers.Dense(num_neurons, activation=tf.nn.relu)(last_layer)
-
-    last_layer = layers.Dense(num_output_neurons, activation=output_activation)(last_layer)
-
-    self.model = tf.keras.Model(inputs=self.input_layer, outputs=last_layer)
+      self.model.add(layers.Dense(num_neurons, activation=tf.nn.relu))
+    
+    self.model.add(layers.Dense(num_output_neurons, activation=output_activation))
 
   def _forward_pass(self, inputs):
     return self.model(inputs)
