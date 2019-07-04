1537360f222eb19223a023a81a9d8a60e8170260
Anton Matosov <anton.matosov@gmail.com>
2019-07-03 23:29:33 -0700

    a bit more cleanup

--- tf_learning/agents/network_agent.py
+++ tf_learning/agents/network_agent.py
@@ -5,14 +5,14 @@ from tf_learning.common import *
 from tf_learning.agents import AbstractAgent
 
 
-EPSILON_MAX = .9
-EPSILON_MIN = .1
-EPSILON_LAST_STEP = 3 * 1000
-EPSILON_SPACE = np.linspace(EPSILON_MAX, EPSILON_MIN, EPSILON_LAST_STEP)
-
 class NetworkAgent(AbstractAgent):
   def __init__(self, env, network, teacher):
     super().__init__(env)
+
+    self.epsilon_max = .9
+    self.epsilon_min = .1
+    self.epsilon_last_step = 3 * 1000
+    self.epsilon_space = np.linspace(self.epsilon_max, self.epsilon_min, self.epsilon_last_step)
 
     self.teacher = teacher
 
@@ -22,20 +22,19 @@ class NetworkAgent(AbstractAgent):
   @property
   def epsilon(self):
     current_step = self.teacher.train_step_counter.numpy()
-    return EPSILON_SPACE[np.minimum(current_step, EPSILON_LAST_STEP - 1)]
+    return self.epsilon_space[np.minimum(current_step, self.epsilon_last_step - 1)]
 
   def select_action(self, observation):
     self.last_observation = observation
+
+    explore = self.teacher.force_explore() or np.random.random() < self.epsilon
     with tf.name_scope('Agent/'):
       tf.compat.v2.summary.scalar(name='epsilon', data=self.epsilon, step=self.teacher.train_step_counter)
+      tf.compat.v2.summary.scalar(name='explore', data=explore, step=self.teacher.train_step_counter)
 
-    if self.teacher.force_explore() or np.random.random() < self.epsilon:
+    if explore:
       self.last_action = self.env.action_space.sample()
-      with tf.name_scope('Agent/'):
-        tf.compat.v2.summary.scalar(name='explore', data=True, step=self.teacher.train_step_counter)
     else:
-      with tf.name_scope('Agent/'):
-        tf.compat.v2.summary.scalar(name='explore', data=False, step=self.teacher.train_step_counter)
       q_values = self.network.forward_pass(np.expand_dims(observation, 0))[0]
       self.last_action = tf.math.argmax(q_values).numpy()
 
